9e7c55277d2189b232277206e23f9bb459fcda7d
luffyguy <harshtanwar25@gmail.com>
2020-12-03 00:22:45 +0530

    Updated calculator

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[0].id)
 
 
 def speak(audio):
@@ -14,11 +14,13 @@ def speak(audio):
 r = sr.Recognizer()
 mic= sr.Microphone(device_index=1)
 with mic as source:
-    speak("Sir! what you want to calculate, example: 3 plus 3")
+    speak("what you want to calculate, example: 3 plus 3")
+    print("what you want to calculate, example: 3 plus 3")
     r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
 query=r.recognize_google(audio)
 
+print(query)
 speak(query)
 
 def operators(op):
@@ -33,7 +35,6 @@ def calculation(op1, oper, op2):
     op1,op2 = int(op1), int(op2)
     return operators(oper)(op1, op2)
 
-print(calculation(*(query.split())))
-
 speak("is equal to")
 speak(calculation(*(query.split())))
+print("is equal to",calculation(*(query.split())))
